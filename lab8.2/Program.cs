4f259e021b8a6a0afa8e7649897bb0bfd7d6ca1f
chemaeva8898@gmail.com <chemaeva8898@gmail.com>
2021-12-05 20:12:26 +1000

    Добавьте файлы проекта.

---
+++ lab8.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace lab8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "10 чисел/Log.txt";
+            using (StreamWriter sw = new StreamWriter(path, true))
+            { 
+                sw.WriteLine("10 случайых чисел: "); 
+             }
+            double[] myarr = new double[10];
+            Random random = new Random();
+
+            for (int i = 0; i < myarr.Length; i++)
+            {
+                myarr[i] = random.Next();
+            }
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                sw.Write(Convert.ToString(myarr));
+            }
+
+        }
+    }
+}
---
+++ lab8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] allfiles = Directory.GetFiles("C:/Users/User/Desktop/Учеба Программированию/8 день 22.11.2021/lab8/lab8/bin/Debug");
+            foreach(string filename in allfiles)
+            {
+                Console.WriteLine(filename);
+            }
+                        {
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
